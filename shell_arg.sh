5a8a5c4d91346bce1f8905a690e231ad70c5e451
username Ankit Kalbande <kalbande@efr.de>
2017-02-21 10:47:04 +0100

    handeling arguments in shell

---
+++ shell_arg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while getopts ":a:p:" opt; do
+  case $opt in
+    a) arg_1="$OPTARG"
+    ;;
+    p) p_out="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+printf "Argument p_out is %s\n" "$p_out"
+printf "Argument arg_1 is %s\n" "$arg_1"
